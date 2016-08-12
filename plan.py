fb50cf82dd125b793aa315e5e46082567d5f78b9
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2016-08-13 00:38:14 +0300

    basic class outline

--- plan.py
+++ plan.py
@@ -1,8 +1,30 @@
-import os
-import sqlite3
+import argparse  # parsing command line
+import json  # storing config in json
+import sqlite3  # storing user plans in sqlite db
+
 
 class Plan(object):
+    """ Expected to store and retrieve user saved tasks with sorta natural language """
     def __init__(self):
         pass
 
+    def _get_config(self):
+        """ Get user config or create one if not exists """
+        pass
 
+    def _init_db(self):
+        """ Creates basic database structure for app """
+        pass
+
+    def _connect_db(self):
+        """ Handles sqlite database connection logic """
+        pass
+
+    def _parse_args(self):
+        """ Parse user input """
+        pass
+
+
+if __name__ == '__main__':
+    plan = Plan()
+    plan()
