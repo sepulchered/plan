92c174d90fb9db779570eeaf5b2dde31d4546279
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2017-06-23 22:07:12 +0300

    use flask instead of async http

--- plan.py
+++ plan.py
@@ -1,37 +1,50 @@
 import os
 import typing
 
+import requests
+from flask import Flask
 from aiohttp import web, request
 
 
 RequestDataType = typing.Dict[str, str]
 
 
-BOT_API_TOKEN = os.environ.get('BOT_API_TOKEN')
+BOT_API_TOKEN = os.environ.get('PLAN_BOT_API_TOKEN', '_plan_bot_token_not_found')
 API_URL = 'https://api.telegram.org/bot'+ BOT_API_TOKEN + '/{}'
+WEBHOOK_URL = '/bot/{}/'.format(BOT_API_TOKEN)
 
 
-async def make_api_request(http_verb:str, api_method:str, data: RequestDataType=None) -> typing.Awaitable:
-    async with request(method=http_verb.capitalize(), url=API_URL.format(api_method), json=data) as resp:
-        return await resp.json()
+def make_api_request(verb, method, data):
+    resp = requests.request(verb.capitalize(), API_URL.format(method), json=data)
+    if resp:
+        return resp.json
+    else:
+        return None
 
 
-async def check_webhook() -> bool:
-    async with make_api_request('get', 'getWebhookInfo') as resp:
-        hook_info = await resp.json()
-        return (hook_info or {}).get('url')
+def is_webhook_set():
+    resp = make_api_request('get', 'getWebhookInfo')
+    if resp:
+        info = resp.json()
+        return (info or {}).get('url')
+    else:
+        return False
 
 
-async def set_webhook() -> bool:
+def set_webhook():
     pass
 
 
-async def handle_request(request: web.Request) -> web.Response:
-    return web.Response(text='response')
+def process_request():
+    pass
 
 
-app = web.Application()
-app.router.add_get('/', handle_request)
+app = Flask(__name__)
+
+@app.route(WEBHOOK_URL)
+def bot_hook(request):
+    return 'gigla'
 
 
-web.run_app(app, port=8666)
+if __name__ == '__main__':
+    pass
