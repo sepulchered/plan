22432ed47478abb0c20c06fa0d36c30998c4072f
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2017-06-24 00:11:37 +0300

    host from os env

--- bot/plan.py
+++ bot/plan.py
@@ -6,8 +6,9 @@ import telepot.loop as pot_loop
 import telepot.delegate as pot_delegate
 
 
-BOT_API_TOKEN = os.environ.get('PLAN_BOT_TOKEN', '')
-BOT_HOOK_URL = os.environ.get('PLAN_BOT_URL', '/bot/hook')
+BOT_API_TOKEN = os.environ.get('BOT_TOKEN', '')
+BOT_HOOK_URL = os.environ.get('BOT_HOOK_URL', '/bot/hook')
+BOT_HOST = os.environ.get('BOT_HOST', '')
 
 # bot related
 class Planner(telepot.helper.ChatHandler):
@@ -15,7 +16,6 @@ class Planner(telepot.helper.ChatHandler):
         super(Planner, self).__init__(*args, **kwargs)
 
     def on_chat_message(self, msg):
-        print('recieved message: "{}"'.format(msg))
         self.sender.sendMessage('gigla')
 
 
@@ -37,21 +37,17 @@ app = flask.Flask(__name__)
 def index():
     return 'planner bot'
 
-@app.route('/bot/hook', methods=['GET', 'POST'])
+@app.route(BOT_HOOK_URL, methods=['GET', 'POST'])
 def on_event():
-    print('webhook has been called')
     webhook.feed(flask.request.data)
     return 'OK'
 
 
 if __name__ == '__main__':
     try:
-        print('trying to set webhook at url "{}"'.format(BOT_HOOK_URL))
-        bot.setWebhook(BOT_HOOK_URL)
+        bot.setWebhook(BOT_HOST+BOT_HOOK_URL)
     except telepot.exception.TooManyRequestsError:
         pass
-    except Exception as ex:
-        print('error while setting webhook', ex)
 
     webhook.run_as_thread()
     app.run()
